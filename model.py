6501a0bd326c496d4cd4c8c912438f39f51f7598
Mitanshu Khandal <mitukhandal01@gmail.com>
2026-08-30 18:15:00 +0530

    Step 27: make_mcts_node

--- model.py
+++ model.py
@@ -297,8 +297,15 @@ def greedy_action_from_policy(logits, mask):
     action = torch.argmax(masked_logits,dim=-1)
     return int(action.item())
 
-# Step 27 - make_mcts_node (not yet solved)
-# TODO: implement
+# Step 27 - make_mcts_node
+def make_mcts_node(prior=0.0, parent=None):
+    return {
+        "prior": float(prior),
+        "visit_count": 0,
+        "value_sum": 0.0,
+        "children": {},
+        "parent": parent,
+    }
 
 # Step 28 - node_q_value (not yet solved)
 # TODO: implement

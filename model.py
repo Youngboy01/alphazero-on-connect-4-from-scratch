0d3206f1e99a0353e7c64ed7e0605d462c159786
Mitanshu Khandal <mitukhandal01@gmail.com>
2026-09-12 00:23:30 +0530

    Step 35: run_one_simulation

--- model.py
+++ model.py
@@ -392,8 +392,26 @@ def backup_value(leaf, value):
         current_value = -current_value
         node = node["parent"]
 
-# Step 35 - run_one_simulation (not yet solved)
-# TODO: implement
+# Step 35 - run_one_simulation
+def run_one_simulation(root, net, c_puct):
+    leaf = select_leaf(root, c_puct)
+
+    board = leaf["board"]
+    to_play= leaf["to_play"]
+
+    done,winner = is_terminal(board)
+    leaf["is_expanded"] = False
+    if done:
+        if winner == 0:
+            value = 0.0
+        elif winner == to_play:
+            value = 1 
+        else:
+            value = -1
+    else:
+        priors,value = evaluate_with_network(net, board, to_play)
+        expand_node(leaf, priors)
+    backup_value(leaf, value)
 
 # Step 36 - run_mcts (not yet solved)
 # TODO: implement

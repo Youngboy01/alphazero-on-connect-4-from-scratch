c9e6b0776d4d9376c1dcf8b0e00e0f9ab9212513
Mitanshu Khandal <mitukhandal01@gmail.com>
2026-09-12 11:31:23 +0530

    Step 40: play_self_play_game

--- model.py
+++ model.py
@@ -468,8 +468,19 @@ def record_self_play_step(history, board, policy, to_play):
     history.append(observation)
     return history
 
-# Step 40 - play_self_play_game (not yet solved)
-# TODO: implement
+# Step 40 - play_self_play_game
+def play_self_play_game(net, num_simulations, c_puct, temperature=1.0):
+    board = make_empty_board()
+    to_play = 1
+    done = False
+    winner = 0
+
+    history = []
+    while not done:
+        action, policy = mcts_choose_action(board, to_play, net, num_simulations, c_puct,temperature)
+        record_self_play_step(history, board, policy, to_play)
+        board, done, winner, to_play = step_env(board, action, to_play)
+    return history, winner
 
 # Step 41 - assign_value_targets (not yet solved)
 # TODO: implement
